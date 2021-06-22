8c2ac0b5ab177b5bcceb8999495072ac58bd3705
Rees <40927062+rees047@users.noreply.github.com>
2021-06-21 23:56:29 -0400

    postman

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const   express = require('express'),
+        bodyParser  = require('body-parser');      
         morgan  = require('morgan');        
         
-const app_express = express();
+const app = express();
 
-let topMovies = [
+let movies = [
     {
         title: 'Avengers: End Game',
         genre: 'Adventure', //fantasy
@@ -46,27 +47,77 @@ let topMovies = [
     }
 ];
 
-app_express.use(morgan('common'));
+app.use(bodyParser.json());
+app.use(morgan('common'));
+app.use(express.static(__dirname + '/public'));
 
-app_express.get('/', (req, res) => {
+app.get('/', (req, res) => {
     res.send('You are on the Home Page!');
 });
 
-app_express.get('/movies', (req, res) => {
-    res.json(topMovies);
-});
-
-app_express.get('/documentation', (req, res) => {
+app.get('/documentation', (req, res) => {
     res.sendFile('public/documentation.html', {
         root: __dirname
     });
 });
 
-app_express.listen(8080, () =>{
+app.get('/movies', (req, res) => {
+    res.json(movies);
+});
+
+app.get('/movies/:title', (req, res) => {
+    res.json(movies.find((movie) => {
+        return movie.title === req.params.title
+    }));
+});
+
+app.get('/movies/:title/genre', (req, res) => {
+    res.send('Return data about genre');
+});
+
+app.get('/movies/:director', (req, res) => {
+    res.json(movies.find((director) => {
+        return movies.director === req.params.director
+    }));
+});
+
+app.post('/users/register', (req, res) => {
+    res.send('Accept data about user registration');
+});
+
+app.put('/users/register/:username', (req, res) => {
+    res.json(users.find((username) => {
+        return users.username === req.params.username
+    }));
+});
+
+app.put('/users/:username/favorites/add/:movie', (req, res) => {
+    res.json(users.find((username) => {
+        return users.username === req.params.username
+    }));
+    res.send('Movie Added!');
+});
+
+app.delete('/users/:username/favorites/remove/:movie', (req, res) => {
+    res.json(users.find((username) => {
+        return users.username === req.params.username
+    }));
+    res.send('Movie Deleted!');
+});
+
+app.delete('/users/deregister/:username', (req, res) => {
+    res.json(users.find((username) => {
+        return users.username === req.params.username
+    }));
+    res.send('User Profile Deleted!');
+});
+
+
+app.listen(8080, () =>{
     console.log('Your app is listening on port 8080');
 });
 
-app_express.use((err, req, rest, next) => {
+app.use((err, req, rest, next) => {
     console.error(err.stack);
     res.status(500).send('Uh-oh. Somethings not working right!');
 });
